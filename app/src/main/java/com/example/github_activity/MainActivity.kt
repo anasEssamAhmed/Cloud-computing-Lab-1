89394fa551a1d87a55f98fb2c18245b260a64979
anasEssamAhmed <anasps201@gmail.com>
2023-02-27 19:00:27 +0200

    First Commit By Anas Ahmed

---
+++ app/src/main/java/com/example/github_activity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.github_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Add The Data To Firestore
+        save.setOnClickListener {
+            var name = myName.text.toString()
+            var email = email.text.toString()
+            var age = age.text.toString()
+            val person = hashMapOf(
+                "name" to name,
+                "email" to email,
+                "age" to age )
+            db.collection("All Person").add(person)
+                .addOnSuccessListener { id ->
+                    Toast.makeText(this,"This is ID : $id" , Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,"I am Sorry This Operation is Failure" , Toast.LENGTH_LONG).show()
+                }
+        }
+
+    }
+}
